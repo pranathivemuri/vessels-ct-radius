f5be6239e9b73508984bfe8cba3cae3874391617
pranathi7 <pranathi93.vemuri@gmail.com>
2016-12-14 13:11:24 -0800

    reconstructed vasculature corrected, dice added

--- vessels/radius_of_vessels.py
+++ vessels/radius_of_vessels.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from scipy import ndimage
-from scipy.misc import imread
+from scipy.misc import imread, imsave
 from skimage import morphology
 
 """
@@ -15,7 +15,7 @@ radius of the vessels in the image obtained using skeleton of 2D image and eucli
 """
 
 
-def _get_boundaries_of_image(binary_image):
+def get_boundaries_of_image(binary_image):
     """
     Return boundaries of binary image
     Parameters
@@ -42,7 +42,8 @@ def _get_boundaries_of_image(binary_image):
 
 def get_radius_by_points_on_skeleton(binary_image, pix_size=None):
     """
-    Returns an image
+    Returns an dictionary, image both contatining radius at a non-zero coordinate
+    on centerline or skeleton
     Parameters
     ----------
     binary_image : 2D array
@@ -68,39 +69,38 @@ def get_radius_by_points_on_skeleton(binary_image, pix_size=None):
     non-zero co-ordinate
     """
     skeleton_image = morphology.skeletonize(binary_image)
-    boundary_image = _get_boundaries_of_image(binary_image)
+    boundary_image = get_boundaries_of_image(binary_image)
     if pix_size is None:
         pix_size = [1] * skeleton_image.ndim
     skeleton_image_copy = copy.deepcopy(skeleton_image)
     start = time.time()
     skeleton_image_copy[skeleton_image == 0] = 255
     skeleton_image_copy[boundary_image == 1] = 0
-    eucledian_radius_image = ndimage.distance_transform_edt(skeleton_image, pix_size)
+    eucledian_radius_image = ndimage.distance_transform_edt(skeleton_image_copy, pix_size)
     list_nzi = list(set(map(tuple, np.transpose(np.nonzero(skeleton_image)))))
     dict_nodes_radius = {item: eucledian_radius_image[item] for item in list_nzi}
     print("time taken to find radius at nodes is %0.3f seconds" % (time.time() - start))
     return dict_nodes_radius, eucledian_radius_image, skeleton_image
 
 
-def get_reconstructed_vasculature(dict_nodes_radius, eucledian_radius_image, skeleton_image, path=None):
+def get_reconstructed_vasculature(dict_nodes_radius, eucledian_radius_image, skeleton_image, path_to_save=None):
     """
-    Return 3 vertex clique removed graph
+    Return 3D volume of vessels reconstructed
     Parameters
     ----------
-    networkxGraph : Networkx graph
-        graph to remove cliques from
+    dict_nodes_radius : dict
+        key: non-zero co-ordinate, value : radius
+    eucledian_radius_image : 2D array
+        2D float array with radius at each point on skeleton, has same shape as binary_image
+    skeleton_image : 2D array
+        skeleton/centerline of binary image of same shape as binary image
+    path_to_save: str
+        absolute path to save the reconstructed 2D image at
 
     Returns
     -------
     networkxGraphAfter : Networkx graph
         graph with 3 vertex clique edges removed
-
-    Notes
-    ------
-    Removes the longest edge in a 3 Vertex cliques,
-    Special case edges are the edges with equal
-    lengths that form the 3 vertex clique.
-    Doesn't deal with any other cliques
     """
     norm_radiuses = get_normalized_values(dict_nodes_radius)
     start = time.time()
@@ -117,6 +117,8 @@ def get_reconstructed_vasculature(dict_nodes_radius, eucledian_radius_image, ske
         color_coded_image[reconstruct_ith_image != 0] = norm_radiuses[index]
         reconstructed_image = np.logical_or(reconstructed_image, reconstruct_ith_image)
     print("time taken to reconstruct the skeleton is %0.3f seconds" % (time.time() - start))
+    if path_to_save is not None:
+        imsave(path_to_save, reconstructed_image)
     return reconstructed_image, color_coded_image
 
 
@@ -146,7 +148,7 @@ def get_normalized_values(dict_nodes_radius):
     return [((radius - min_radius) / (max_radius - min_radius)) * 255 for radius in radiuses]
 
 
-def get_radius_coded_color_vessels(dict_nodes_radius, shape_image, path=None,):
+def get_radius_coded_color_vessel_skeleton(dict_nodes_radius, shape_image, path=None):
     """
     Return 3 vertex clique removed graph
     Parameters
@@ -210,6 +212,38 @@ def plot_results(*args, **kwargs):
     return fig
 
 
+def compute_dice_coeffiecient(segmented_vasculature, reconstructed_vasculature, empty_score=1.0):
+    """
+    Computes the Dice coefficient, a measure of set similarity.
+    Parameters
+    ----------
+    segmented_vasculature : 2D or 3D Boolean array
+        Any array of arbitrary size.
+    reconstructed_vasculature : 2D or 3D Boolean array
+        Any other array of identical size as segmented_vasculature
+    Returns
+    -------
+    dice : float
+        Dice coefficient as a float on range [0,1].
+        Maximum similarity = 1
+        No similarity = 0
+        Both are empty (sum eq to zero) = empty_score
+    Notes
+    -----
+    The order of inputs is commutative.
+    See https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
+    for more understanding
+    """
+    if segmented_vasculature.shape != reconstructed_vasculature.shape:
+        raise ValueError("Shape mismatch: segmented_vasculature and reconstructed_vasculature must have the same shape")
+    im_sum = segmented_vasculature.sum() + reconstructed_vasculature.sum()
+    if im_sum == 0:
+        return empty_score
+    # Compute Dice coefficient
+    intersection = np.logical_and(segmented_vasculature, reconstructed_vasculature)
+    return 2. * intersection.sum() / im_sum
+
+
 if __name__ == '__main__':
     path = input("enter a path to the CT Projection of vessels------")
     input_image = imread(path)
@@ -218,9 +252,9 @@ if __name__ == '__main__':
     dict_nodes_radius, eucledian_radius_image, skeleton_image = get_radius_by_points_on_skeleton(input_image, pix_size)
     reconstructed_image = get_reconstructed_vasculature(eucledian_radius_image, skeleton_image)
     path_to_save = os.path.split(path)[0] + os.sep
-    color_coded_image = get_radius_coded_color_vessels(dict_nodes_radius,
-                                                       path_to_save + "radius_coded_color_vessels.png",
-                                                       skeleton_image.shape)
+    color_coded_image = get_radius_coded_color_vessel_skeleton(dict_nodes_radius,
+                                                               path_to_save + "radius_coded_color_vessels.png",
+                                                               skeleton_image.shape)
     plt_kwargs_dict = {'titles': ['original binary image','skeleton of original image','reconstructed image']}
     plt_kwargs_dict['path'] = path_to_save + "reconstructed_vessels.png"
     plot_results(input_image, skeleton_image, reconstructed_image, **plt_kwargs_dict)
