a08fb8577e0de1a345c81d69c756e9a0946678f6
pranathivemuri <pranathi93.vemuri@gmail.com>
2017-02-14 11:35:08 -0800

    wip:tests

--- vessels/radius_skeleton.py
+++ vessels/radius_skeleton.py
@@ -1,5 +1,4 @@
 import copy
-import os
 
 import numpy as np
 
@@ -56,7 +55,7 @@ def get_radius_2d(binary_image, skeleton_image, boundary_image, pix_size=None):
         boundaries of objects in binary_image
 
     pix_size : list
-        list of 2 variables giving voxel size or pixel size, giving resolution in xy, x
+        list of 2 variables giving voxel size or pixel size, giving resolution in x, y
 
     Returns
     -------
@@ -77,6 +76,68 @@ def get_radius_2d(binary_image, skeleton_image, boundary_image, pix_size=None):
     list_nzi = map(tuple, np.transpose(np.nonzero(skeleton_image)))
     dict_nodes_radius = {item: eucledian_radius_image[item] for item in list_nzi}
     return dict_nodes_radius
+
+
+def get_radius_slicewise(binary_vol, skeleton_vol, boundary_vol, pix_size=[1, 1], plane=0):
+    """
+    Returns a dictionary, image both contatining radius at a non-zero coordinate
+    on centerline or skeleton
+    Parameters
+    ----------
+    binary_vol : 3D array
+        binary image of (m, n, k) shape
+
+    skeleton_vol : 3 array
+        skeletonized image of binary_image of (m, n, k) shape
+
+    boundary_vol : 3D array
+        boundaries of objects in binary_image
+
+    pix_size : list
+        list of 3 variables giving voxel size or pixel size, giving resolution in z, x, y
+
+    plane : int
+        plane = 0 look in x, y cross-section of the volume
+        plane = 1 look in z, y cross-section of the volume
+        plane = 2 look in z, x cross-section of the volume
+
+    Returns
+    -------
+    dict_nodes_radius : dict
+        key: non-zero co-ordinate, value : radius
+    """
+    dict_nodes_radius = {}
+    for i in range(skeleton_vol.shape[plane]):
+        if plane == 0:
+            dict_nodes_radius.update(get_radius_2d(binary_vol[i, :, :], skeleton_vol[i, :, :],
+                                                   boundary_vol[i, :, :], pix_size))
+        elif plane == 1:
+            dict_nodes_radius.update(get_radius_2d(binary_vol[:, i, :], skeleton_vol[:, i, :],
+                                                   boundary_vol[:, i, :], pix_size))
+        elif plane == 2:
+            dict_nodes_radius.update(get_radius_2d(binary_vol[:, :, i], skeleton_vol[:, :, i],
+                                                   boundary_vol[:, :, i], pix_size))
+    return dict_nodes_radius
+
+
+def get_max_dict(list_of_dicts):
+    """
+    Return 2D or 3D array of vessels reconstructed
+    Parameters
+    ----------
+    list_of_dicts : list
+        list of dicts
+
+    Returns
+    -------
+    max_dict : dict
+        given a list of dicts having common keys,
+        one dict is returned with maximum value at the common keys
+    """
+    max_dict = {}
+    for key, val in list_of_dicts[0].items():
+        max_dict[key] = max(list_of_dicts[i][key] for i in range(len(list_of_dicts)))
+    return max_dict
 
 
 def get_reconstructed_vasculature(dict_nodes_radius, shape):
@@ -109,120 +170,29 @@ def get_reconstructed_vasculature(dict_nodes_radius, shape):
     return reconstructed_image
 
 
-def get_normalized_values(values, normalize_upper_limit=1):
-    """
-    Return normalized list of elements
-    Parameters
-    ----------
-    values : list
-        list of elements to be normalized
-
-    normalize_upper_limit : int
-        upper limit to which normalize values should be scaled to, default 1
-    Returns
-    -------
-    list
-        list of normalized values scaled between 0 and normalize_upper_limit
-
-    Notes
-    -----
-    Normalized as val - min_val / (max_val - min_val)
-    """
-    min_value = min(values)
-    max_value = max(values)
-    return [((value - min_value) / (max_value - min_value)) * normalize_upper_limit for value in values]
-
-
-def get_radius_coded_color_vessel_skeleton(dict_nodes_radius, shape):
-    """
-    Return 3 vertex clique removed graph
-    Parameters
-    ----------
-    networkxGraph : Networkx graph
-        graph to remove cliques from
-
-    Returns
-    -------
-    networkxGraphAfter : Networkx graph
-        graph with 3 vertex clique edges removed
-
-    Notes
-    ------
-    Removes the longest edge in a 3 Vertex cliques,
-    Special case edges are the edges with equal
-    lengths that form the 3 vertex clique.
-    Doesn't deal with any other cliques
-    """
-    color_coded_image = np.zeros(shape, dtype=np.float32)
-    norm_radiuses = get_normalized_values(dict_nodes_radius.values(), 255)
-    for dest, radius in zip(list(dict_nodes_radius.keys()), norm_radiuses):
-        color_coded_image[dest] = radius
-    return color_coded_image
-
-
-def get_radius_slicewise(binary_vol, skeleton_vol, boundary_vol, pix_size=[1, 1], plane=0):
-    """
-    Returns a dictionary, image both contatining radius at a non-zero coordinate
-    on centerline or skeleton
-    Parameters
-    ----------
-    binary_image : 2D array
-        binary image of (m, n) shape
-
-    skeleton_image : 2D array
-        skeletonized image of binary_image of (m, n) shape
-
-    boundary_image : 2D array
-        boundaries of objects in binary_image
-
-    pix_size : list
-        list of 2 variables giving voxel size or pixel size, giving resolution in xy, x
-
-    Returns
-    -------
-    dict_nodes_radius : dict
-        key: non-zero co-ordinate, value : radius
-       (z, x, y) removes voxels with radius 0.0 and get radius by looking in the plane = 0 look in x, y
-       plane = 1 look in z, x and plane = 2 look in z, y
-    """
-    dict_nodes_radius = {}
-    for i in range(skeleton_vol.shape[plane]):
-        if plane == 0:
-            dict_nodes_radius.update(get_radius_2d(binary_vol[i, :, :], skeleton_vol[i, :, :],
-                                                   boundary_vol[i, :, :], pix_size))
-        elif plane == 1:
-            dict_nodes_radius.update(get_radius_2d(binary_vol[:, i, :], skeleton_vol[:, i, :],
-                                                   boundary_vol[:, i, :], pix_size))
-        elif plane == 2:
-            dict_nodes_radius.update(get_radius_2d(binary_vol[:, :, i], skeleton_vol[:, :, i],
-                                                   boundary_vol[:, :, i], pix_size))
+def get_radius_3d(binary_vol, skeleton_vol, boundary_vol, pix_size):
+    d = [0] * 3
+    for i in range(3):
+        d[i] = get_radius_slicewise(binary_vol, skeleton_vol, boundary_vol, pix_size, i)
+    dict_nodes_radius = get_max_dict(d)
     return dict_nodes_radius
 
 
-def max_radius(d):
-    max_radius_dict = {}
-    for key, val in d[0].items():
-        max_radius_dict[key] = max(d[0][key], d[1][key], d[2][key])
-    return max_radius_dict
-
-
 if __name__ == '__main__':
-    path = input("enter a path to the CT Projection of vessels------")
+    path = input("enter a path to the 2D array of vessels------")
     binary_image = imread(path)
-    pix_size = input("please enter resolution of a pixels in 2D with resolution in y and x")
+    pix_size = input("please enter resolution of a pixels in 2D with resolution in x and y")
     pix_size = [float(item) for item in pix_size.split(' ')]
-    dict_nodes_radius = get_radius_2d(binary_image, pix_size)
+    skeleton_image = morphology.skeletonize_2d(binary_image)
+    boundary_image = get_boundaries_of_image(binary_image)
+    dict_nodes_radius = get_radius_2d(binary_image, skeleton_image, boundary_image, pix_size)
     reconstructed_image = get_reconstructed_vasculature(dict_nodes_radius, binary_image.shape)
-    path_to_save = os.path.split(path)[0] + os.sep
-    color_coded_image = get_radius_coded_color_vessel_skeleton(dict_nodes_radius,
-                                                               path_to_save + "radius_coded_color_vessels.png",
-                                                               binary_image.shape)
+
     path = input("enter a path to 3D binary vasculature volume")
     binary_vol = np.load(path)
+    pix_size = input("please enter resolution of a pixels in 3D with resolution in z, x, and y")
+    pix_size = [float(item) for item in pix_size.split(' ')]
     skeleton_vol = thin_volume.get_thinned(binary_vol)
     boundary_vol = get_boundaries_of_image(binary_vol)
-    d = [0] * 3
-    for i in range(3):
-        d[i] = get_radius_slicewise(binary_vol, plane=i)
-    max_radius_dict = max_radius(d)
-    rv = get_reconstructed_vasculature(max_radius_dict, binary_vol.shape)
+    dict_nodes_radius = get_radius_3d(binary_vol, skeleton_vol, boundary_vol, pix_size)
+    reconstructed_volume = get_reconstructed_vasculature(dict_nodes_radius, binary_vol.shape)
---
+++ vessels/radius_skeleton_tests.py
@@ -0,0 +1,34 @@
+import nose.tools
+from skimage import morphology
+
+import vessels.radius_skeleton as radius_skeleton
+import kesm.analysis.phantoms.vessel_phantom as vesse_phantom
+import kesm.analysis.skeleton.thin_volume as thin_volume
+
+
+def _helper_radius():
+    pass
+
+
+def test_get_boundaries_of_image():
+    pass
+
+
+def test_get_radius_2d():
+    pass
+
+
+def test_get_radius_slicewise():
+    pass
+
+
+def test_get_max_dict():
+    pass
+
+
+def test_get_reconstructed_vasculature():
+    pass
+
+
+def test_get_radius_3d():
+    pass
